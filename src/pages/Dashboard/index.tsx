4db8c611344f6e1316d30ede7aede1094c012b3b
Stephen Goncalves <stiefo.o@gmail.com>
2020-09-06 06:10:26 +0100

    :rocket: Implemented Dashboard and repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import React, { useState, FormEvent, useEffect } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import api from '../../services/api'
+
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories, Error } from './style'
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>
+
+    const [newRepo, setNewRepo] = useState('');
+
+    const [inputError, setInputError] = useState('');
+
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories)
+        )
+    }, [repositories])
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Seriously? Impossible search for a blank "user/repo"')
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+
+            setNewRepo('');
+
+            setInputError('');
+        } catch (error) {
+            setInputError('Repo and/or User may not exist, please check the input and try again!')
+        }
+
+    }
+
+    return (
+        <>
+            <img src={logoImg} alt="Github Exporer" />
+            <Title>Explore Github's repositories</Title>
+
+            <Form hasError={Boolean(inputError)} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositorio" />
+                <button type="submit">Search</button>
+            </Form>
+
+            {inputError && <Error>{inputError}</Error>}
+
+            <Repositories>
+                {repositories.map(repository => (
+                    <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </Link>
+                ))}
+            </Repositories>
+        </>
+    )
 }
 
 export default Dashboard
